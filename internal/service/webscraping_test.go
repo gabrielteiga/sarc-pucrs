be3c6a8ef8af7d8ef310dddd4099933289e65e62
Gabriel Teiga <gabrielteiga99@gmail.com>
2025-04-29 14:38:05 -0300

    tests: use ExpectedResult instead ExpectedResponse in tests

--- internal/service/webscraping_test.go
+++ internal/service/webscraping_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type ExpectedResponse struct {
+type ExpectedResult struct {
 	ID       string
 	Subject  string
 	Group    string
@@ -19,11 +19,11 @@ type ExpectedResponse struct {
 func TestGetScheduleByURL(t *testing.T) {
 	tests := []struct {
 		Url      string
-		Expected ExpectedResponse
+		Expected ExpectedResult
 	}{
 		{
 			Url: "https://sarc.pucrs.br/Default/Export.aspx?id=c37c8651-287e-4b65-a0b8-a286a6eb69be&ano=2025&sem=1",
-			Expected: ExpectedResponse{
+			Expected: ExpectedResult{
 				ID:       "c37c8651-287e-4b65-a0b8-a286a6eb69be",
 				Subject:  "Linguagens de Programação",
 				Group:    "31",
@@ -35,7 +35,7 @@ func TestGetScheduleByURL(t *testing.T) {
 		},
 		{
 			Url: "https://sarc.pucrs.br/Default/Export.aspx?id=25b9fe09-df0b-47c0-8abf-d6a570871f6b&ano=2025&sem=1",
-			Expected: ExpectedResponse{
+			Expected: ExpectedResult{
 				ID:       "25b9fe09-df0b-47c0-8abf-d6a570871f6b",
 				Subject:  "Verificação e Validação de Software",
 				Group:    "30",
@@ -47,7 +47,7 @@ func TestGetScheduleByURL(t *testing.T) {
 		},
 		{
 			Url: "https://sarc.pucrs.br/Default/Export.aspx?id=e9448fa0-3507-41fe-be4c-f1d8b9a32d43&ano=2025&sem=1",
-			Expected: ExpectedResponse{
+			Expected: ExpectedResult{
 				ID:       "e9448fa0-3507-41fe-be4c-f1d8b9a32d43",
 				Subject:  "Fundamentos de Redes de Computadores",
 				Group:    "30",
@@ -64,7 +64,7 @@ func TestGetScheduleByURL(t *testing.T) {
 	}
 }
 
-func runTest(t *testing.T, url string, expected ExpectedResponse) {
+func runTest(t *testing.T, url string, expected ExpectedResult) {
 	scheduleMap := GetScheduleByURL(url)
 
 	for key, value := range scheduleMap {
